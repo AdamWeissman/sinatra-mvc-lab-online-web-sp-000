3ce8915a9e48e0c9628714f3003a3ad0740291ae
Adam Weissman <adam@comedytragedyepic.com>
2019-12-20 10:31:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/piglatinize' do 
-    
-    @piglatinized_text = PigLatinizer.new
-    
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
+
 end
